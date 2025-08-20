5fcdafa669d2b77bf2ebd731e610c03422c7fd4a
marcuszxc <115086076+marcuszxc@users.noreply.github.com>
2025-08-20 14:21:29 +0200

    Update setup_nvim.sh

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -21,3 +21,13 @@ mkdir /etc/xdg/nvim
 mkdir /etc/xdg/nvim/lua
 mkdir /etc/xdg/nvim/lua/config
 mkdir /etc/xdg/nvim/lua/plugins
+
+# Fix a global node install directory to make mcp happy
+sudo mkdir -p /usr/local/lib/node_modules_global
+sudo groupadd odoo
+# Add your user to the group (and other users as needed)
+sudo usermod -a -G odoo $USER
+# Change the ownership of the directory to the new group
+sudo chown -R root:npmusers /usr/local/lib/node_modules_global
+# Set the permissions to allow the group to write
+sudo chmod -R 775 /usr/local/lib/node_modules_global
