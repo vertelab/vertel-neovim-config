528f175711362bdb607c82c3cb2d665f2e4c03c4
marcuszxc <115086076+marcuszxc@users.noreply.github.com>
2025-08-21 15:46:37 +0200

    Added nvim files and directorys

--- setup_server_nvim.sh
+++ setup_server_nvim.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 USER_LOCATION=$(pwd)
+NVIM=/etc/xdg/nvim
 
 # snap installing neovim 
 sudo snap install nvim
@@ -17,10 +18,26 @@ sudo luarocks install luasocket
 cd $USER_LOCATION
 
 # setting up global neovim config
-mkdir /etc/xdg/nvim
-mkdir /etc/xdg/nvim/lua
-mkdir /etc/xdg/nvim/lua/config
-mkdir /etc/xdg/nvim/lua/plugins
+mkdir $NVIM
+mkdir $NVIM/mcphub
+mkdir $NVIM/lua/config
+mkdir $NVIM/lua/plugins
+mkdir $NVIM/lua/themes
+
+# get nvim files
+wget -O $NVIM/init.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/init.lua
+wget -O $NVIM/mcphub/servers.json https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/mcphub/servers.json
+wget -O $NVIM/lua/config/lazy.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/lua/config/lazy.lua
+wget -O $NVIM/lua/plugins/codecompanion.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/lua/plugins/codecompanion.lua
+wget -O $NVIM/lua/plugins/comment.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/lua/plugins/comment.lua
+wget -O $NVIM/lua/plugins/diffview.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/lua/plugins/diffview.lua
+wget -O $NVIM/lua/plugins/harpoon.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/lua/plugins/harpoon.lua
+wget -O $NVIM/lua/plugins/lsp.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/lua/plugins/lsp.lua
+wget -O $NVIM/lua/plugins/mason.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/lua/plugins/mason.lua
+wget -O $NVIM/lua/plugins/mcphub.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/lua/plugins/mcphub.lua
+wget -O $NVIM/lua/plugins/telescope.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/lua/plugins/telescope.lua
+wget -O $NVIM/lua/plugins/which-key.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/lua/plugins/which-key.lua
+wget -O $NVIM/lua/themes/init.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/lua/themes/init.lua
 
 # Fix a global node install directory to make mcp happy
 sudo mkdir -p /usr/local/lib/node_modules_global
