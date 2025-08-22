e1d4a9bbcb8dff541dbe49a1fdafdce9e80ab4a4
marcuszxc <115086076+marcuszxc@users.noreply.github.com>
2025-08-22 13:50:51 +0200

    Update setup_host_nvim.sh

--- setup_host_nvim.sh
+++ setup_host_nvim.sh
@@ -1,7 +1,42 @@
-NVIM=/home/$USER/.config/nvim
-mkdir /home/$USER/.config
+#!/bin/bash
+
+NVIM=$HOME/.config/nvim
+
+# Snap installing neovim 
+sudo snap install nvim --classic
+
+# Fix latest version of nodejs for mcphub
+sudo DEBIAN_FRONTEND=noninteractive apt-get remove -y nodejs libnode-dev nodejs-doc
+sudo apt update
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs
+
+# Installing luarocks dependencys and more
+sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+    -o Dpkg::Options::="--force-confdef" \
+    -o Dpkg::Options::="--force-confold" \
+    lua5.1 liblua5.1-dev liblua5.1-0-dev unzip ripgrep mercurial build-essential
+
+# setup tree-sitter
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+cargo install tree-sitter-cli
+
+# Seting up and installing luarocks
+cd /tmp
+wget -O /tmp/luarocks.tar.gz https://luarocks.org/releases/luarocks-3.12.2.tar.gz
+tar zxpf luarocks.tar.gz
+cd /tmp/luarocks-3.12.2/
+./configure && make && sudo make install
+sudo luarocks install luasocket
+sudo rm ../luarocks* -r
+cd $USER_LOCATION
+
+mkdir $HOME/.config
 mkdir $NVIM
 mkdir $NVIM/mcphub
+mkdir $NVIM/lua
 mkdir $NVIM/lua/config
 mkdir $NVIM/lua/plugins
 mkdir $NVIM/lua/themes
@@ -19,3 +54,13 @@ wget -O $NVIM/lua/plugins/mcphub.lua https://github.com/vertelab/vertel-neovim-c
 wget -O $NVIM/lua/plugins/telescope.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/lua/plugins/telescope.lua
 wget -O $NVIM/lua/plugins/which-key.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/lua/plugins/which-key.lua
 wget -O $NVIM/lua/themes/init.lua https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/nvim/lua/themes/init.lua
+
+# Fix a global node install directory to make mcp happy
+sudo mkdir -p /usr/local/lib/node_modules_global
+# Change the ownership of the directory to the new group
+sudo chown -R $USER:$USER /usr/local/lib/node_modules_global
+# Set the permissions to allow the group to write
+sudo chmod -R 775 /usr/local/lib/node_modules_global
+sudo npm config set prefix /usr/local/lib/node_modules_global --global
+sudo wget -O /etc/profile.d/set_nvim_env_variables.sh https://github.com/vertelab/vertel-neovim-config/raw/refs/heads/main/set_nvim_env_variables.sh
+. /etc/profile.d/set_nvim_env_variables.sh
