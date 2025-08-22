af3f4dbf85926a3793678c7a026ba2ac5f6243c3
marcuszxc <marcus.karlsson@vertel.se>
2025-08-22 06:59:32 +0000

    Added dependency nvim-tree/nvim-web-devicons to diffview

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,7 +1,7 @@
 return {
     {
       "sindrets/diffview.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
+      dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
       config = function()
         require("diffview").setup()
         -- Optional key mapping to open the diff view
